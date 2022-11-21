e40147e290cb0a4497cd82eac896bf7ad989392a
marianalG0 <garciahaumariana@gmail.com>
2022-11-20 23:08:32 -0500

    segunda parte
    Videos del 3 al 6

---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+  templateUrl: './empleado.component.html',
+  //template: "<p>Aqui ira un empleado</p>",
+  styleUrls: ['./empleado.component.css']
+  //styles: ["p{background-color:red;}"]
+})
+export class EmpleadoComponent {
+
+}
---
+++ src/app/empleados/empleados.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core"; //Linea de importacion para poder utilizar component 
+
+@Component({
+    selector: "app-empleados",
+    templateUrl: "./empleados.component.html",// Es una ruta relativa
+    styleUrls: ['./empleados.component.css'] 
+})
+
+export class EmpleadosComponent{
+
+}
